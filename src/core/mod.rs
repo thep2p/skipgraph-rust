060fdb58d5743b4c37c2a35f0f748f11aa97327d
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-03-28 08:57:29 -0700

    exports lookup table externally accessible

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -7,6 +7,7 @@ mod node;
 pub use crate::core::model::address::Address;
 pub use crate::core::model::identifier::Identifier;
 pub use crate::core::model::memvec::MembershipVector;
+pub use crate::core::lookup::lookup_table::LookupTable;
 pub use crate::core::search::id_search_req::IdentifierSearchRequest;
 pub use crate::core::search::id_search_res::IdentifierSearchResult;
 pub use crate::core::node::Node;
