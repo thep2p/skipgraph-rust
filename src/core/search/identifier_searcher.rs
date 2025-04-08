8b9d2f36bf564fa3c0bb365d81a097e69792f83f
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-04-08 10:50:27 -0700

    removes unused import

--- src/core/search/identifier_searcher.rs
+++ src/core/search/identifier_searcher.rs
@@ -1,4 +1,3 @@
-use crate::core::model::identity::Identity;
 use crate::core::lookup::lookup_table::LookupTable;
 use crate::core::search::id_search_req::IdentifierSearchRequest;
 use crate::core::search::id_search_res::IdentifierSearchResult;
@@ -6,10 +5,13 @@ use crate::core::search::id_search_res::IdentifierSearchResult;
 trait IdentifierSearcher<T> {
     /// Performs the search for given identifier in the lookup table in the given direction and level.
     /// Essentially looks for the first match in the direction for the given level and all levels below.
-    /// The match is the first entry that is greater than or equal to the target identifier (for left direction), 
+    /// The match is the first entry that is greater than or equal to the target identifier (for left direction),
     /// or less than or equal to the target identifier (for right direction).
     /// Returns the search result.
     /// If the lookup table is empty in that direction, returns None.
-    fn search_by_id(&self, lookup_table: &dyn LookupTable<T>, search_req : IdentifierSearchRequest) -> anyhow::Result<IdentifierSearchResult<T>>;
+    fn search_by_id(
+        &self,
+        lookup_table: &dyn LookupTable<T>,
+        search_req: IdentifierSearchRequest,
+    ) -> anyhow::Result<IdentifierSearchResult<T>>;
 }
-
