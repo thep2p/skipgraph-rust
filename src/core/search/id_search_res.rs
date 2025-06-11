3b5d2f52ae95ff62272ecd3dd6ffcd8057d5005a
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-06-11 13:17:52 -0700

    Refactors IdentifierSearchResult struct
    Refactors the `IdentifierSearchResult` struct to decouple it
    from the request object and instead store the target identifier
    and level directly. This improves clarity and reduces
    unnecessary coupling.

--- src/core/search/id_search_res.rs
+++ src/core/search/id_search_res.rs
@@ -1,19 +1,29 @@
+use crate::core::Identifier;
+use crate::core::lookup::lookup_table::LookupTableLevel;
 use crate::core::model::identity::Identity;
 use crate::core::search::id_search_req::IdentifierSearchRequest;
 
 pub struct IdentifierSearchResult<T> {
-    // TODO: req should be decomposed into direction and level
-    req : IdentifierSearchRequest,
+    target: Identifier,
+    level: LookupTableLevel,
     result: Option<Identity<T>>,
 }
 
 impl<T> IdentifierSearchResult<T> {
     pub fn new(req: IdentifierSearchRequest, result: Option<Identity<T>>) -> Self {
-        IdentifierSearchResult { req, result }
+        IdentifierSearchResult {
+            target: req.target,
+            level: req.level,
+            result,
+        }
     }
 
-    pub fn request(&self) -> &IdentifierSearchRequest {
-        &self.req
+    pub fn target(&self) -> &Identifier {
+        &self.target
+    }
+
+    pub fn level(&self) -> LookupTableLevel {
+        self.level
     }
 
     pub fn result(&self) -> Option<&Identity<T>> {
