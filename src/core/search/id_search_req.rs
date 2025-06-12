a71f470b6df76f13481df84ddb79f67438998802
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-06-12 16:10:04 -0700

    Defines Identifier Search Request struct
    Introduces a struct to encapsulate parameters for
    searching identifiers within the lookup table.
    This struct includes the target identifier, the lookup
    table level, and the search direction.
    Provides getter methods for accessing the fields.

--- src/core/search/id_search_req.rs
+++ src/core/search/id_search_req.rs
@@ -3,7 +3,29 @@ use crate::core::model::direction::Direction;
 use crate::core::Identifier;
 
 pub struct IdentifierSearchRequest {
-    pub target : Identifier,
-    pub level : LookupTableLevel,
-    pub direction : Direction,
+    target : Identifier,
+    level : LookupTableLevel,
+    direction : Direction,
+}
+
+impl IdentifierSearchRequest {
+    pub fn new(target: Identifier, level: LookupTableLevel, direction: Direction) -> Self {
+        IdentifierSearchRequest {
+            target,
+            level,
+            direction,
+        }
+    }
+
+    pub fn target(&self) -> &Identifier {
+        &self.target
+    }
+
+    pub fn level(&self) -> LookupTableLevel {
+        self.level
+    }
+
+    pub fn direction(&self) -> Direction {
+        self.direction
+    }
 }
