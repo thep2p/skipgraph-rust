3fdaa5c5e3e69968e7e9d5f87e3a7d7a197a905d
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-07-10 14:01:23 -0700

    Adds IdentifierSearchResult struct
    Introduces a struct to encapsulate the results of identifier searches within lookup tables.
    
    This struct includes the target identifier, the lookup table level where the search terminated, and the resulting identifier found.
    It provides a structured way to manage and access identifier search results.

--- src/core/search/id_search_res.rs
+++ src/core/search/id_search_res.rs
@@ -1,6 +1,18 @@
 use crate::core::lookup::lookup_table::LookupTableLevel;
 use crate::core::Identifier;
 
+/// A struct representing the result of an identifier search within lookup table of current node.
+///
+/// The `IdentifierSearchResult` struct is composed of three key components:
+/// - The `target` identifier that was searched for in the lookup table of the current node.
+/// - The `level` of the lookup table where the identifier search was terminated at the current
+/// node.
+/// - The `result` identifier that was found during the search process at the current node.
+///
+/// # Derives
+///
+/// This struct derives the `Debug` trait, enabling it to be formatted using the `{:?}` formatter
+/// for debugging purposes.
 #[derive(Debug)]
 pub struct IdentifierSearchResult {
     target: Identifier,
@@ -9,6 +21,20 @@ pub struct IdentifierSearchResult {
 }
 
 impl IdentifierSearchResult {
+    /// Constructs a new `IdentifierSearchResult` instance.
+    ///
+    /// # Parameters
+    ///
+    /// - `target`: An `Identifier` representing the target element for the search operation in the
+    /// lookup table of the current node.
+    /// - `level`: A `LookupTableLevel` that specifies the lookup level where the search was terminated
+    ///  at the current node.
+    /// - `result`: An `Identifier` holding the result of the search operation at the current node.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `IdentifierSearchResult` instance populated with the provided `target`, `level`,
+    /// and `result` parameters.
     pub fn new(target: Identifier, level: LookupTableLevel, result: Identifier) -> Self {
         IdentifierSearchResult {
             target,
@@ -17,14 +43,17 @@ impl IdentifierSearchResult {
         }
     }
 
+    /// Returns a reference to the `target` field of the struct.
     pub fn target(&self) -> &Identifier {
         &self.target
     }
 
+    /// Returns the level of the lookup table where the search was terminated at the current node.
     pub fn level(&self) -> LookupTableLevel {
         self.level
     }
 
+    /// Returns the result of the search operation at the current node.
     pub fn result(&self) -> &Identifier {
         &self.result
     }
