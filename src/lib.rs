8e8d720347378a2dacd2c8c367cd96ef442f8875
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-07-17 11:46:21 -0700

    Introduces network communication layer
    Adds a network module defining the interfaces and structures
    for network communication. This includes message types, a
    message struct, a message processor trait, and a network trait
    with methods for sending messages, registering processors, and
    starting/stopping the network service.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod core;
 mod local;
+mod network;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
---
+++ src/network/mod.rs
@@ -0,0 +1,34 @@
+use std::any::Any;
+
+/// MessageType enum defines the types of messages that can be sent over the network.
+pub enum MessageType {
+    TestMessage(String), // A message for testing purposes, it is a simple string message, and is not used in production.
+}
+
+/// Message struct represents a message that can be sent over the network.
+pub struct Message {
+    pub message_type: MessageType,
+    pub payload: Box<dyn Any + Send>
+}
+
+/// MessageProcessor trait defines the entity that processes the incoming network messages at this node.
+pub trait MessageProcessor: Send + Sync {
+    fn process_incoming_message(&mut self, message: Message) -> anyhow::Result<()>;
+}
+
+/// Network trait defines the interface for a network service that can send and receive messages.
+pub trait Network: Send + Sync {
+    /// Sends a message to the network.
+    fn send_message(&self, message: Message) -> anyhow::Result<()>;
+
+    /// Registers a message processor to handle incoming messages. 
+    /// At any point in time, there can be only one processor registered.
+    /// Registering a new processor is illegal if there is already a processor registered, and causes an error.
+    fn register_processor(&mut self, processor: Box<dyn MessageProcessor>) -> anyhow::Result<()>;
+
+    /// Starts the network service.
+    fn start(&mut self) -> anyhow::Result<()>;
+
+    /// Stops the network service.
+    fn stop(&mut self) -> anyhow::Result<()>;
+}
