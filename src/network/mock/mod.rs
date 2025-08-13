d341bb3755419015254b809addbeee2c86440339
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-08-13 15:58:17 -0700

    feat: Introduce No-Op Network for Testing

--- src/network/mock/mod.rs
+++ src/network/mock/mod.rs
@@ -1,4 +1,7 @@
+
 pub mod network;
 pub mod hub;
 #[cfg(test)]
 mod network_test;
+#[cfg(test)]
+pub mod noop_network;
--- src/network/mock/network.rs
+++ src/network/mock/network.rs
@@ -1,7 +1,7 @@
-use crate::network::mock::hub::NetworkHub;
 use crate::network::{Message, MessageProcessor, Network};
 use anyhow::Context;
 use std::sync::{Arc, Mutex};
+use crate::network::mock::hub::NetworkHub;
 
 /// MockNetwork is a mock implementation of the Network trait for testing purposes.
 /// It does not perform any real network operations but simulates message routing and processing through a `NetworkHub`.
